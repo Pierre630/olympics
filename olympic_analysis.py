3bbd06d5f7b399e9534587c1af1ebab98d488fed
Pierre630 <114083527+Pierre630@users.noreply.github.com>
2024-06-05 23:50:59 +0000

    fixed typ in olympic.cvs

--- olympic_analysis.py
+++ olympic_analysis.py
@@ -1,5 +1,6 @@
-import streamlit as st
+#import streamlit as st
 import pandas as pd
 
-olympic_chart_data = pd.read_csv('olypics.csv', usecols=['0', '12', '13', '14', '15'])
+st.write("hello")
+olympic_chart_data = pd.read_csv('olympics.csv', usecols=['0', '12', '13', '14', '15'])
 print(olympic_chart_data)
