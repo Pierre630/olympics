ba2149958ae7fa80f1ae1e46f6f997b503dcfbbf
Pierre630 <114083527+Pierre630@users.noreply.github.com>
2024-06-06 00:06:08 +0000

    added heading "Olympic Medal Data"

--- olympic_analysis.py
+++ olympic_analysis.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
 
+st.write("Olympic Medal Data")
 olympic_chart_data = pd.read_csv('olympics.csv', usecols=['0', '12', '13', '14', '15'])
 print(olympic_chart_data)
