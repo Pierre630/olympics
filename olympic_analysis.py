156d9f3c894d5d3ea6c23bf534d53b503d552c29
Pierre630 <114083527+Pierre630@users.noreply.github.com>
2024-06-06 01:48:54 +0000

    attempting to make a bargraph

--- olympic_analysis.py
+++ olympic_analysis.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.write("Olympic Medal Data")
 olympic_chart_data = pd.read_csv('olympics.csv', usecols=['0', '12', '13', '14', '15'])
-st.write(olympic_chart_data)
 st.table(olympic_chart_data)
+st.bar_chart(olympic_chart_data)
